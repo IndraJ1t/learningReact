c418f17f1cd255893477fd0f1a172fb80e617ac4
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-08-08 17:06:42 +0530

    01_Hooks

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -2,26 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 function App() {
+
   let [counter, setCounter] = useState(10)
   function addValue() {
-    // if (counter > 20) {
-
-    // }
     setCounter(counter + 1)
   }
-  function removeValue() {
-    // if (counter < 0) {
 
-    // }
+  function removeValue() {
     setCounter(counter - 1)
   }
+
   return (
     <>
       <h1>Let's start</h1>
       <button onClick={addValue} disabled={counter >= 20}>Add value {counter}</button>
       <br />
-      <button onClick={removeValue} disabled={0 == counter}>Remove vlaue{counter}</button>
+      <button onClick={removeValue} disabled={0 == counter}>Remove vlaue {counter}</button>
 
     </>
   );
