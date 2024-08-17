c849e023a9797ef3cba96e3c518d121ec85895fe
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-08-18 00:58:11 +0530

    added content

--- 01-hooks/src/App.jsx
+++ 01-hooks/src/App.jsx
@@ -6,13 +6,29 @@ import './App.css'
 function App() {
 
   let [counter, setCounter] = useState(10)
+
   function addValue() {
     setCounter(counter + 1)
   }
 
+  {/*
+    function addValue() {
+    // setCounter = (counter + 1)
+    setCounter((prevCounter) => {
+      return prevCounter + 1;
+    })
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1) // this will update value of counter +4 for every click
+
+  }  // setCounter also recieves a call back function if needed to perform multiple same tasks at once
+  // because useState sends the updates in bulk batches which would treat (counter + 1) as a single task 
+  // even if multiple times as above in comment.
+  */}
   function removeValue() {
     setCounter(counter - 1)
   }
+
 
   return (
     <>
@@ -22,6 +38,8 @@ function App() {
       <button onClick={removeValue} disabled={0 == counter}>Remove vlaue {counter}</button>
 
     </>
-  );
+  )
 }
+
+
 export default App
